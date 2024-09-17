affc975c96c97bed11540d40ef8a2d9b5ea004e1
fatima-tuz-zahra-123 <163109907+fatima-tuz-zahra-123@users.noreply.github.com>
2024-09-17 18:16:45 +0000

    Describe your changes here

---
+++ lab1task1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    
+    std::cout << "The size of int is: " << sizeof(int) << " bytes" << std::endl;
+    std::cout << "The size of int is: " << sizeof(long) << " bytes" << std::endl;
+    std::cout << "The size of int is: " << sizeof(double) << " bytes" << std::endl;
+    std::cout << "The size of int is: " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "The size of int is: " << sizeof(char) << " bytes" << std::endl;
+
+    return 0;
+}
